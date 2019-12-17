24b3916a8fcb51a0b10b0aa5c9c4378554d67e7a
yusufabdillahputra <yusufabdillahputra@gmail.com>
2019-12-17 16:33:58 +0700

    Modul Country

--- src/app/models/country.js
+++ src/app/models/country.js
@@ -1,24 +1,31 @@
-const { post, get } = require('../../helper/db')
+const { post, get, search } = require('../../helper/db')
+
 const table = 'tbl_country'
 const primaryKey = 'id_country'
 
 module.exports = {
-  createData : async (req) => {
+  createData: async (req) => {
     return new Promise((resolve, reject) => {
       const prepare = {
-        sql : `INSERT INTO ${table} SET ?`,
-        values : req.body
+        sql: `INSERT INTO ${table} SET ?`,
+        values: req.body
       }
       post(prepare, resolve, reject)
     })
   },
-  readAll : async (req) => {
+  readAll: async (req) => {
     return new Promise((resolve, reject) => {
-      const prepare = {
-        sql : `SELECT * FROM ${table} ORDER BY name_country ASC`
+      if (req.query.fn) {
+        const prepare = search(req, table, primaryKey, 'name_country')
+        get(prepare, resolve, reject)
+      } else {
+        const prepare = {
+          sql: `SELECT * FROM ${table} ORDER BY name_country ASC`
+        }
+        get(prepare, resolve, reject)
       }
-      get(prepare, resolve, reject)
     })
-  }
+  },
+
 
 }
--- src/helper/db.js
+++ src/helper/db.js
@@ -1,12 +1,12 @@
 require('dotenv/config')
+const sprintf = require('sprintf-js').sprintf
 const conn = require('../config/mysql')
 
 module.exports = {
   conn,
   get: (prepare, resolve, reject) => {
-    conn.query(prepare.sql, function (error, result) {
+    conn.query(prepare.sql, prepare.values, function (error, result) {
       if (error) reject(error)
-      console.log(result)
       resolve(result)
     })
   },
@@ -27,5 +27,26 @@ module.exports = {
       if (error) reject(error)
       resolve(result)
     })
+  },
+  search: (req, table, primaryKey, defaultFieldname) => {
+    const value = {
+      fieldvalue: '%' + req.query.fv + '%' || '% %',
+      limit: req.query.limit || 60,
+      offset: req.query.offset || 0
+    }
+    const property = {
+      fieldname: req.query.fn || defaultFieldname,
+      orderBy: req.query.order || primaryKey,
+      sort: req.query.sort || 'ASC'
+    }
+    const sqlString = sprintf(`SELECT * FROM ${table} WHERE %(fieldname)s LIKE ? ORDER BY %(orderBy)s %(sort)s LIMIT ? OFFSET ?`, property)
+    return {
+      sql: sqlString,
+      values: [
+        value.fieldvalue,
+        value.limit,
+        value.offset
+      ]
+    }
   }
 }
