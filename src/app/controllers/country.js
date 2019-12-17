0862cefb0fa5506f76929609db31f88d23f27ccc
yusufabdillahputra <yusufabdillahputra@gmail.com>
2019-12-17 15:44:00 +0700

    Modul Country

--- src/app/controllers/country.js
+++ src/app/controllers/country.js
@@ -14,6 +14,7 @@ module.exports = {
   readAll: async (req, res) => {
     try {
       const model = await countryModel.readAll(req)
+      console.log(model)
       await response(res, 200, 200, model)
     } catch (error) {
       response(res, 200, 200, error)
